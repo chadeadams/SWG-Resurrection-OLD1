b2447606a8a6ad497807af16582b7474c41d1427
nugax <chadeadams@gmail.com>
2017-01-03 16:04:41 -0600

    Updated Glowy Requirements
    updated requirements

--- MMOCoreORB/bin/scripts/screenplays/jedi/shrine/ForceShrineMenuComponent.lua
+++ MMOCoreORB/bin/scripts/screenplays/jedi/shrine/ForceShrineMenuComponent.lua
@@ -70,6 +70,8 @@ function ForceShrineMenuComponent:doMeditate(pObject, pPlayer)
 
 
 --Added Code for SWG Resurrection Jedi Code:
+
+-- =============== Orginial Code ======================
 --[[	if (not isJediPadawan) then
 --		-- Unlock Padawan
 --		if (CreatureObject(pPlayer):hasScreenPlayState(64, "VillageJediProgression")) then
@@ -91,20 +93,7 @@ function ForceShrineMenuComponent:doMeditate(pObject, pPlayer)
         if (not isJediPadwan) then
         --Unlock Padawan / check to see if they have iniate rank from killing mellachie
         if (CreatureObject(pPlayer):hasSkill("force_title_jedi_rank_01")) then
-             --[  awardSkill(pPlayer, "force_title_jedi_rank_02")
-              -- CreatureObject(pPlayer):playEffect("clienteffect/trap_electric_01.cef", "")
-	          -- CreatureObject(pPlayer):playMusicMessage("sound/music_become_jedi.snd")
-              -- CreatureObject(pPlayer):sendSystemMessage("@quest/force_sensitive/intro:completed_padawan") 
-              
-                --Give Jedi Padawan Robes
-               -- if not (self:hasFullInventory(pPlayer)) then
-		       -- local pInventory = CreatureObject(pPlayer):getSlottedObject("inventory")
-		       -- local pItem = giveItem(pInventory, self.jediPadawanRobe, -1)
-	           -- else
-		          --  CreatureObject(pPlayer):sendSystemMessage("@jedi_spam:inventory_full_jedi_robe")
-	            --end
-               --]
-              	if (pPlayer == nil) then
+               	if (pPlayer == nil) then
 		           return
 	            end
 
