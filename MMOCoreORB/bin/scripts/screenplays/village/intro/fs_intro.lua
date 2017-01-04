a3f2c343e10a511f6335175dbba251cdff2e534b
nugax <chadeadams@gmail.com>
2017-01-04 12:16:46 -0600

    Updated FSIntro

--- MMOCoreORB/bin/scripts/screenplays/village/intro/fs_intro.lua
+++ MMOCoreORB/bin/scripts/screenplays/village/intro/fs_intro.lua
@@ -17,8 +17,8 @@ FsIntro = ScreenPlay:new {
 	     --[1] = { 300, 600 }, -- Old man visit, 5-10 mins for testing
     	 --[3] = { 300, 600 } -- Sith shadow attack, 5-10 mins for testing
           --Testing Code For Times     
-           [1]= { 60,120} - Old Man,
-           [3] = { 60, 120} - Sith 
+           [1] = { 60, 120}, -- Old Man
+           [3] = { 60, 120}  -- Sith 
 	}
 }
 
