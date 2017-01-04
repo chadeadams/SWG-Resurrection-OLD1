842f5d86774ede464a7581bc76e43c4311a60156
nugax <chadeadams@gmail.com>
2017-01-04 10:20:44 -0600

    Revert "Advanced Healing Terminals"
    This reverts commit ae4e329aa2749b32526710ba7beaf8162b3d662d.

--- MMOCoreORB/bin/scripts/screenplays/buffterminals/advanced_buff_terms.lua
+++ MMOCoreORB/bin/scripts/screenplays/buffterminals/advanced_buff_terms.lua
@@ -1,36 +1,40 @@
--- Buff Terminals
--- Documentation: doc/features/add-buff-terminals.md
+-- Buff Terminal
+-- Sells players a variety of buffs
+-- Buff values can be changed in the buff table. 
+-- More buffs can be added by copy/pasting an existing buff table entry and changing the values in the copy.
+-- To disable a terminal or buff, simply comment it out or remove it from the table.
+-- New C++ Lua calls added for this terminal: 
+-- LuaCreatureObject::removeBuffs, LuaCreatureObject::emptyStomach, LuaCreatureObject::getActivePetsSize, LuaCreatureObject::getActivePet
 
-TarkinBuffTerminal = ScreenPlay:new {
+BuffTerminal = ScreenPlay:new {
 	numberOfActs = 1,
-	healingFee = 2000, -- Optional fee (in Credits) for healing wounds and battle fatigue
-	buffPets = 1, -- 0 = No, 1 = Yes. Will buff all the player's active pets Health/Action/Mind with the same boost as the player.
+	healingFee = 0, -- Optional fee (in Credits) for healing wounds and battle fatigue
+	buffPets = 0, -- 0 = No, 1 = Yes. Will buff all the player's active pets Health/Action/Mind with the same boost as the player.
 	buffPetSecondaryStats = 0, -- 0 = No, 1 = Yes. Will also buff the pet's secondary stats.
 	buffs = {
 		-- Legend: {name, price, duration in hours, health, strength, constitution, action, quickness, stamina, mind, focus, willpower},
-		{"Starter Buff", 0, 4, 500, 100, 50, 200, 100, 50, 200, 100, 50, requiredSkills = {}, requiredSkillNames = ""}, -- Free!
-		{"Basic Buff", 5000, 3, 800, 300, 150, 500, 300, 150, 350, 200, 100, requiredSkills = {}, requiredSkillNames = ""},
-		{"Advanced Buff", 15000, 2, 1100, 500, 250, 800, 500, 250, 500, 250, 150, requiredSkills = {"", ""}, requiredSkillNames = ""},
-		{"Uber Buff", 25000, 1, 1500, 750, 250, 800, 750, 250, 800, 350, 200, requiredSkills = {"", ""}, requiredSkillNames = ""},
+		{"Starter Buff", 0, 2, 500, 250, 250, 500, 250, 250, 500, 250, 250}, -- Free!
+		{"Basic Buff", 10000, 2, 650, 500, 400, 650, 500, 400, 650, 500, 400},
+		{"Advanced Buff", 100000, 3, 800, 600, 600, 800, 600, 600, 800, 600, 600},
+		{"Halloween Uber Event Buffs", 0, 4, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000}, --Used For Special Events
 	},
 	termModel = "object/tangible/terminal/terminal_light_enclave_voting.iff",
-	termName = "Medical Services Terminal",
+	termName = "Halloween Event Medical Services Terminal",
 	terminals = {
-		{planetName = "tatooine", x = -32.7499, z = 0.258537, y = 3.52207, ow = 0.729551, oy = 0.683926, cellID = 9995780}, -- Mos Espa Hospital
-		{planetName = "tatooine", x = 7.73135, z = 0.26, y = -1.69846, ow = 0.353085, oy = 0.935591, cellID = 9995699}, -- Mos Espa Hospital
-		{planetName = "tatooine", x = -2932.59, z = 5.0, y = 2117.02, ow = 0.899624, oy = 0.436665, cellID = 0}, -- Mos Espa starting area 
+		{planetName = "yavin4", x = -293.367, z = 35, y = 4854.52, ow = 0.999974, oy = 0.00721678}, -- Yavin4
+    
 	}
 }
 
-registerScreenPlay("TarkinBuffTerminal", true)
+registerScreenPlay("BuffTerminal", true)
 
-function TarkinBuffTerminal:start()
+function BuffTerminal:start()
 	-- Spawn terminals
 	for i = 1, #self.terminals, 1 do
-		local pTerminal = spawnSceneObject(self.terminals[i].planetName, self.termModel , self.terminals[i].x, self.terminals[i].z, self.terminals[i].y, self.terminals[i].cellID, self.terminals[i].ow, 0, self.terminals[i].oy, 0)
+		local pTerminal = spawnSceneObject(self.terminals[i].planetName, self.termModel , self.terminals[i].x, self.terminals[i].z, self.terminals[i].y, 0, self.terminals[i].ow, 0, self.terminals[i].oy, 0)
 		if (pTerminal ~= nil) then
 			-- Add menu and custom name
-			SceneObject(pTerminal):setObjectMenuComponent("TarkinBuffTerminalMenuComponent")
+			SceneObject(pTerminal):setObjectMenuComponent("BuffTerminalMenuComponent")
 			SceneObject(pTerminal):setCustomObjectName(self.termName)
 		end
 	end
@@ -39,8 +43,8 @@ end
 
 -- UI Window Functions
 
-function TarkinBuffTerminal:openWindow(pCreatureObject, pUsingObject)
-	local sui = SuiListBox.new("TarkinBuffTerminal", "defaultCallback")
+function BuffTerminal:openWindow(pCreatureObject, pUsingObject)
+	local sui = SuiListBox.new("BuffTerminal", "defaultCallback")
 
 	-- Using object ID, stored in PageData
 	if (pUsingObject == nil) then
@@ -87,7 +91,7 @@ function TarkinBuffTerminal:openWindow(pCreatureObject, pUsingObject)
 	sui.sendTo(pCreatureObject)
 end
 
-function TarkinBuffTerminal:defaultCallback(pPlayer, pSui, eventIndex, args)
+function BuffTerminal:defaultCallback(pPlayer, pSui, eventIndex, args)
 	local cancelPressed = (eventIndex == 1)
 
 	if (cancelPressed) then
@@ -115,13 +119,13 @@ end
 
 -- Buff Functions
 
-function TarkinBuffTerminal:removePlayerBuffs(pPlayer)
+function BuffTerminal:removePlayerBuffs(pPlayer)
 	CreatureObject(pPlayer):removeBuffs()
 	CreatureObject(pPlayer):emptyStomach()
 	CreatureObject(pPlayer):sendSystemMessage("Your buffs have been removed and your stomach is now empty.")
 end
 
-function TarkinBuffTerminal:removePetBuffs(pPlayer)
+function BuffTerminal:removePetBuffs(pPlayer)
 	local numberOfPets = CreatureObject(pPlayer):getActivePetsSize()
 		
 	if (numberOfPets > 0) then
@@ -136,7 +140,7 @@ function TarkinBuffTerminal:removePetBuffs(pPlayer)
 	end
 end
 
-function TarkinBuffTerminal:healWounds(pPlayer)
+function BuffTerminal:healWounds(pPlayer)
 	local price = self.healingFee
 
 	if (price > 0) then
@@ -156,7 +160,7 @@ function TarkinBuffTerminal:healWounds(pPlayer)
 	CreatureObject(pPlayer):sendSystemMessage("Your wounds have been healed and your mind is at ease.")
 end
 
-function TarkinBuffTerminal:applyBuff(pPlayer, buffSelected)
+function BuffTerminal:applyBuff(pPlayer, buffSelected)
 	local duration = self.buffs[buffSelected][3] * 60  * 60
 	local price = self.buffs[buffSelected][2]
 	
@@ -215,15 +219,15 @@ end
 
 -- Radial Menu Functions
 
-TarkinBuffTerminalMenuComponent = { }
+BuffTerminalMenuComponent = { }
 
-function TarkinBuffTerminalMenuComponent:fillObjectMenuResponse(pSceneObject, pMenuResponse, pPlayer)
+function BuffTerminalMenuComponent:fillObjectMenuResponse(pSceneObject, pMenuResponse, pPlayer)
 	local menuResponse = LuaObjectMenuResponse(pMenuResponse)
 	menuResponse:addRadialMenuItem(20, 3, "Use Terminal")
 	menuResponse:addRadialMenuItem(120, 3, "About Buffs")
 end
 
-function TarkinBuffTerminalMenuComponent:handleObjectMenuSelect(pObject, pPlayer, selectedID)
+function BuffTerminalMenuComponent:handleObjectMenuSelect(pObject, pPlayer, selectedID)
 	if (pPlayer == nil or pObject == nil) then
 		return 0
 	end
@@ -234,31 +238,31 @@ function TarkinBuffTerminalMenuComponent:handleObjectMenuSelect(pObject, pPlayer
 	end
 	
 	if (selectedID == 20) then
-		TarkinBuffTerminal:openWindow(pPlayer, pObject)
+		BuffTerminal:openWindow(pPlayer, pObject)
 	elseif (selectedID == 120) then
 		local suiManager = LuaSuiManager()
 		local helpMessage = "This window shows the stats that are increased for each buff type, as well as their price and duration.\n\n"
 		
-		for i = 1, #TarkinBuffTerminal.buffs, 1 do
-			helpMessage = helpMessage .. TarkinBuffTerminal.buffs[i][1] .. " (" .. tostring(TarkinBuffTerminal.buffs[i][2]) .. "cr/" .. tostring(TarkinBuffTerminal.buffs[i][3]) .. "h):\n" -- BuffName (1234cr/2h):
+		for i = 1, #BuffTerminal.buffs, 1 do
+			helpMessage = helpMessage .. BuffTerminal.buffs[i][1] .. " (" .. tostring(BuffTerminal.buffs[i][2]) .. "cr/" .. tostring(BuffTerminal.buffs[i][3]) .. "h):\n" -- BuffName (1234cr/2h):
 			helpMessage = helpMessage .. "- - - - - - - - - - - - -\n"
-			helpMessage = helpMessage .. "Health: " .. TarkinBuffTerminal.buffs[i][4] .. "\n"
-			helpMessage = helpMessage .. "Strength: " .. TarkinBuffTerminal.buffs[i][5] .. "\n"
-			helpMessage = helpMessage .. "Constitution: " .. TarkinBuffTerminal.buffs[i][6] .. "\n"
-			helpMessage = helpMessage .. "Action: " .. TarkinBuffTerminal.buffs[i][7] .. "\n"
-			helpMessage = helpMessage .. "Quickness: " .. TarkinBuffTerminal.buffs[i][8] .. "\n"
-			helpMessage = helpMessage .. "Stamina: " .. TarkinBuffTerminal.buffs[i][9] .. "\n"
-			helpMessage = helpMessage .. "Mind: " .. TarkinBuffTerminal.buffs[i][10] .. "\n"
-			helpMessage = helpMessage .. "Focus: " .. TarkinBuffTerminal.buffs[i][11] .. "\n"
-			helpMessage = helpMessage .. "Willpower: " .. TarkinBuffTerminal.buffs[i][12] .. "\n\n"
+			helpMessage = helpMessage .. "Health: " .. BuffTerminal.buffs[i][4] .. "\n"
+			helpMessage = helpMessage .. "Strength: " .. BuffTerminal.buffs[i][5] .. "\n"
+			helpMessage = helpMessage .. "Constitution: " .. BuffTerminal.buffs[i][6] .. "\n"
+			helpMessage = helpMessage .. "Action: " .. BuffTerminal.buffs[i][7] .. "\n"
+			helpMessage = helpMessage .. "Quickness: " .. BuffTerminal.buffs[i][8] .. "\n"
+			helpMessage = helpMessage .. "Stamina: " .. BuffTerminal.buffs[i][9] .. "\n"
+			helpMessage = helpMessage .. "Mind: " .. BuffTerminal.buffs[i][10] .. "\n"
+			helpMessage = helpMessage .. "Focus: " .. BuffTerminal.buffs[i][11] .. "\n"
+			helpMessage = helpMessage .. "Willpower: " .. BuffTerminal.buffs[i][12] .. "\n\n"
 		end
 		
-		suiManager:sendMessageBox(pObject, pPlayer, "About Buffs", helpMessage, "@ok", "TarkinBuffTerminalMenuComponent", "noCallback")
+		suiManager:sendMessageBox(pObject, pPlayer, "About Buffs", helpMessage, "@ok", "BuffTerminalMenuComponent", "noCallback")
 	end 
 	
 	return 0
 end
 
-function TarkinBuffTerminalMenuComponent:noCallback(pPlayer, pSui, eventIndex)
+function BuffTerminalMenuComponent:noCallback(pPlayer, pSui, eventIndex)
 	-- do nothing
 end
