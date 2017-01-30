3185bf23bae7689245b3e765b6b2887dd9a76000
nugax <chadeadams@gmail.com>
2017-01-30 07:04:00 -0600

    Enabled Double XP
    Double XP

--- MMOCoreORB/bin/scripts/managers/player_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_manager.lua
@@ -51,10 +51,22 @@ performanceDuration = 7200 -- in seconds
 medicalDuration = 7200 -- in seconds
 
 --Sets the experience multiplier while grouped
-groupExpMultiplier = 2.0
+--Normal
+--groupExpMultiplier = 2.0
+
+--Advanced
+groupExpMultiplier = 4.0
+
 
 --Sets a global experience multiplier
-globalExpMultiplier = 1.0
+--Normal XP
+--globalExpMultiplier = 1.0
+
+--Double XP
+globalExpMultiplier = 2.0
+
+--Triple XP
+--globalExpMultiplier = 3.0
 
 --Sets the base number of control devices of each type that a player can have in their datapad at once
 --For creature pets, A Creature Handler will have the base number + their stored pets skill mod as limit
