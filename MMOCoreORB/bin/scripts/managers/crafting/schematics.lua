07fc8ca5d08aaa8fce9947d54825cb35572c0430
nugax <chadeadams@gmail.com>
2017-02-05 05:49:40 -0600

    Fixed Schematics.Lua

--- MMOCoreORB/bin/scripts/managers/crafting/schematics.lua
+++ MMOCoreORB/bin/scripts/managers/crafting/schematics.lua
@@ -1810,7 +1810,6 @@ schematics = {
 	{path="object/draft_schematic/furniture/bestine/painting_bestine_ronka.iff"},
 	{path="object/draft_schematic/item/theme_park/alderaan/act2/dead_eye_decoder.iff"},
 	{path="object/draft_schematic/item/quest_item/fs_medic_puzzle_heal_pack.iff"},
-    {path="object/draft_schematic/vehicle/civilian/speederbike_old.iff"},
     --Added for new object - Nugax   
     {path="object/draft_schematic/vehicle/civilian/sith_speeder.iff"},
     {path="object/draft_schematic/vehicle/civilian/barc_speeder.iff"},
