fa4cec9c507b9a2ccc37c307d1c77b507c6febfd
nugax <chadeadams@gmail.com>
2017-02-23 07:59:11 -0600

    Fixed Chat Manager Code Syntax

--- MMOCoreORB/src/server/chat/ChatManagerImplementation.cpp
+++ MMOCoreORB/src/server/chat/ChatManagerImplementation.cpp
@@ -975,7 +975,7 @@ void ChatManagerImplementation::broadcastGalaxy(const String& message, const Str
 
 void ChatManagerImplementation::broadcastGalaxy(CreatureObject* player, const String& message) {
 	//String firstName = "SKYNET";
-    String firstName = "SWG Resurrection Server Bot"
+    String firstName = "SWG_RESURRECTION";
 
 	if (player != NULL)
 		firstName = player->getFirstName();
