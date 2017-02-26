ee0482661aa34b9184955638e6c1559336b036cb
nugax <chadeadams@gmail.com>
2017-02-26 14:17:44 -0600

    Updated PAS Staff Member

--- MMOCoreORB/bin/scripts/staff/levels/pas.lua
+++ MMOCoreORB/bin/scripts/staff/levels/pas.lua
@@ -1,13 +1,11 @@
 pas = {
-	level = 12,
+	level = 16,
 	name = "pas",
 	tag = "SWG Resurrection PAS",
 	skills = {
 		"admin_base",
-		"admin_debug_01",
 		"admin_general_admin_01",
 		"admin_general_admin_02",
-		"admin_jedi_management_01",
 		"admin_player_management_01",
 		"admin_player_management_02",
 		"admin_quest_management_01",
