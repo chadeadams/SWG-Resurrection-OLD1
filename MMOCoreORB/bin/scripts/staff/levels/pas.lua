07a193ae91e8ba4f74a674c6c9220eabd5dfc29e
nugax <chadeadams@gmail.com>
2017-02-27 06:20:23 -0600

    Added additional staff permissions to PAS level

--- MMOCoreORB/bin/scripts/staff/levels/pas.lua
+++ MMOCoreORB/bin/scripts/staff/levels/pas.lua
@@ -1,3 +1,9 @@
+--Added player staff level for SWG Resurrection
+--Level Name: Player Account Specialist
+--Added by: nugax (nugax@swgresurrection.com)
+--Date: 2/27/2017
+
+
 pas = {
 	level = 16,
 	name = "pas",
@@ -8,6 +14,8 @@ pas = {
 		"admin_general_admin_02",
 		"admin_player_management_01",
 		"admin_player_management_02",
+        "admin_player_management_03",
+        "admin_player_management_04",
 		"admin_quest_management_01",
 		"admin_server_admin_01"
 
