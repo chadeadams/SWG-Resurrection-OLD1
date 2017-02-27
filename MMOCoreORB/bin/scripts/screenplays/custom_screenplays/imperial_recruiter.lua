71ac7a8664061410cf0b3b5dfc807d8f94bf9f72
nugax <chadeadams@gmail.com>
2017-02-26 20:01:29 -0600

    test

--- MMOCoreORB/bin/scripts/screenplays/custom_screenplays/imperial_recruiter.lua
+++ MMOCoreORB/bin/scripts/screenplays/custom_screenplays/imperial_recruiter.lua
@@ -7,7 +7,7 @@ registerScreenPlay("spawn_custom_imp_recruiters", true)
 
 --This gets executed automatically when the screenplay is started
 function spawn_custom_imp_recruiters:start()
-   local pImpies = spawnMobile("lok", "imperial_recruiter", 1,-211, 12, 3716, -1, 0)
+   local pImpies = spawnMobile("lok", "black_market_dealer", 1,-211, 12, 3716, -1, 0)
 end
 
 --Put all the initial spawning of mobiles in a nice method like this.
